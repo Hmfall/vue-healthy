fa8705f421744a096bb0333940acbb43fbc7d36b
Hmfall <148013305+Hmfall@users.noreply.github.com>
2024-05-18 18:56:30 +0300

    Вынесение в константы

--- src/shared/utils/useStorage.spec.ts
+++ src/shared/utils/useStorage.spec.ts
@@ -1,6 +1,11 @@
 import { afterEach, describe, expect, it } from 'vitest';
-import type { AppSetting, Product } from '@/shared/types';
+import type { AppSetting, DateRange, Product, Theme } from '@/shared/types';
 import useStorage from '@/shared/utils/useStorage';
+
+const productA: Product = { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 };
+const productB: Product = { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 };
+const productC: Product = { name: 'productC', calories: 0, proteins: 0, fats: 0, carbs: 0 };
+const stats: DateRange = { start: '20240420', end: '20240425' };
 
 describe('useStorage', () => {
   afterEach(() => {
@@ -35,9 +40,9 @@ describe('useStorage', () => {
 
       expect(store.get()).toEqual({ theme: 'dark' });
 
-      store.set({ stats: { start: '20240420', end: '20240425' } });
+      store.set({ stats });
 
-      expect(store.get()).toEqual({ stats: { start: '20240420', end: '20240425' } });
+      expect(store.get()).toEqual({ stats });
     });
 
     it('should be set value with merge option equals true correctly', () => {
@@ -45,129 +50,69 @@ describe('useStorage', () => {
 
       expect(store.get()).toEqual({ theme: 'dark' });
 
-      store.set({ stats: { start: '20240420', end: '20240425' } }, { merge: true });
+      store.set({ stats }, { merge: true });
 
-      expect(store.get()).toEqual({ theme: 'dark', stats: { start: '20240420', end: '20240425' } });
+      expect(store.get()).toEqual({ theme: 'dark', stats });
     });
-
-    // it('should be set value with raw option correctly', () => {
-    //   store.set({ theme: 'dark' }, { raw: true });
-    //
-    //   expect(store.get()).toEqual({ theme: 'dark' });
-    // });
   });
 
   describe('useStorage array prototype', () => {
     const store = useStorage<Product[]>('store');
 
     it('should be set value with merge option equals false correctly', () => {
-      store.set([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      store.set([productA, productB]);
 
-      expect(store.get()).toEqual([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      expect(store.get()).toEqual([productA, productB]);
 
-      store.set([{ name: 'productC', calories: 0, proteins: 0, fats: 0, carbs: 0 }]);
+      store.set([productC]);
 
-      expect(store.get()).toEqual([
-        { name: 'productC', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      expect(store.get()).toEqual([productC]);
     });
 
     it('should be set value with merge option equals true correctly', () => {
-      store.set([{ name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 }]);
+      store.set([productA]);
 
-      expect(store.get()).toEqual([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      expect(store.get()).toEqual([productA]);
 
-      store.set(
-        [
-          { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-          { name: 'productC', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        ],
-        { merge: true },
-      );
+      store.set([productB, productC], { merge: true });
 
-      expect(store.get()).toEqual([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productC', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      expect(store.get()).toEqual([productA, productB, productC]);
     });
 
     it('should be set value in case 1 with raw option correctly', () => {
-      store.set(
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { merge: true, raw: true },
-      );
+      store.set(productA, { merge: true, raw: true });
 
-      expect(store.get()).toEqual([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      expect(store.get()).toEqual([productA]);
     });
 
     it('should be set value in case 2 with raw option correctly', () => {
-      store.set([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      store.set([productA, productB]);
 
-      expect(store.get()).toEqual([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      expect(store.get()).toEqual([productA, productB]);
 
-      store.set(
-        { name: 'productC', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { merge: true, raw: true },
-      );
+      store.set(productC, { merge: true, raw: true });
 
-      expect(store.get()).toEqual([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productC', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      expect(store.get()).toEqual([productA, productB, productC]);
     });
 
     it('should be set value in case 3 with raw option correctly', () => {
-      store.set([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      store.set([productA, productB]);
 
-      expect(store.get()).toEqual([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      expect(store.get()).toEqual([productA, productB]);
 
-      store.set({ name: 'productC', calories: 0, proteins: 0, fats: 0, carbs: 0 }, { raw: true });
+      store.set(productC, { raw: true });
 
-      expect(store.get()).toEqual([
-        { name: 'productC', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      expect(store.get()).toEqual([productC]);
     });
 
     it('should be set value in case 4 with raw option correctly', () => {
-      store.set([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      store.set([productA, productB]);
 
-      expect(store.get()).toEqual([
-        { name: 'productA', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-        { name: 'productB', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      expect(store.get()).toEqual([productA, productB]);
 
-      store.set({ name: 'productC', calories: 0, proteins: 0, fats: 0, carbs: 0 }, { raw: true });
+      store.set(productC, { raw: true });
 
-      expect(store.get()).toEqual([
-        { name: 'productC', calories: 0, proteins: 0, fats: 0, carbs: 0 },
-      ]);
+      expect(store.get()).toEqual([productC]);
     });
   });
 });
